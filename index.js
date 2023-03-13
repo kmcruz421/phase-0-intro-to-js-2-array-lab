9b2c68512506fa7b2bfea795350f5cf0f6672541
Karla Cruz <kmcruz421@gmail.com>
2023-03-12 21:33:19 -0400

    added/removed elements dest and nondest

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const catsAddedEnd = [...cats, name];
+    return catsAddedEnd;
+}
+
+function prependCat(name){
+    const catsAddedBeginning = [name, ...cats];
+    return catsAddedBeginning;
+}
+
+function removeLastCat(){
+    const catsWithoutLast = cats.slice(0, cats.length-1);
+    return catsWithoutLast;
+}
+
+function removeFirstCat(){
+    const catsWithoutFirst = cats.slice(1);
+    return catsWithoutFirst;
+}
